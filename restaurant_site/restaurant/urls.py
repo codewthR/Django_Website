75e9ad35faca18721804867eeb78ddf69f96ad17
codewthr <rahulbhatia.ramm@gmail.com>
2024-12-05 18:00:34 +0530

    New Web Changes

--- restaurant_site/restaurant/urls.py
+++ restaurant_site/restaurant/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from django.http import HttpResponse
+
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
+    path('', views.homepage, name='home'),
+    path('menu/', views.menu, name='menu'),
+    # path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
--- restaurant_site/restaurant/views.py
+++ restaurant_site/restaurant/views.py
@@ -1,7 +1,16 @@
 
 # Create your views here.
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def homepage(request):
     return render(request, 'all.html')
+
+def contact(request):
+    return  render(request, 'contact.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+
 
